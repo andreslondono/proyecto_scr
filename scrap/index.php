2d2ded0aaa1a721a0c2e124cc7a6119b3a083834
andreslondono <andres.londono@viaevolution.com>
2017-02-21 12:46:34 +0100

    Probando

--- scrap/index.php
+++ scrap/index.php
@@ -13,7 +13,7 @@ and open the template in the editor.
         <?php
         // put your code here
         
-        echo "hola ¿que tal?";
+        echo "hola ¿que tal? ";
         ?>
     </body>
 </html>
